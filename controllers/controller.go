f30c25c3cdab7b2d6c4394ea7fcd0b351859674e
jiangxm9 <1041965419@qq.com>
2018-11-16 01:37:12 +0800

    基于beego框架的cloudgo

--- controllers/controller.go
+++ controllers/controller.go
@@ -5,10 +5,11 @@ import (
 )
 
 type MainController struct {
-	beego.Controller
+	beego.Controller //继承beego中的Controller类
 }
 
-func (this *MainController) Get() {
-	this.TplName = "tmnt.html"
-	this.Render()
+//重写Get方法，这里参照beego中的todo实例，设置浏览器访问的是之前其他课实现的静态页面
+func (this *MainController) Get() { 
+	this.TplName = "tmnt.html" //this.Tpl就是需要渲染的模板，这里会默认读取views文件夹中的文件
+	this.Render() //调用beego已实现的Render函数渲染
 }
--- main.go
+++ main.go
@@ -6,6 +6,6 @@ import (
 )
 
 func main() {
-	beego.Router("/", &controllers.MainController{})
-	beego.Run(":9527")
+	beego.Router("/", &controllers.MainController{})// 设置路由，传入controller处理
+	beego.Run(":9527") //开放9527端口运行
 }
